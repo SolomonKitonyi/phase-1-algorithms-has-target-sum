383a5736df9f16194d0b7fd976462a158ba9db1c
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-08-19 09:57:28 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Write your algorithm here((
+    let map = {};
+    for(let i = 0; i < array.length; i ++) {
+        if(map[array[i]] !== undefined){
+            return true;
+        }
+        else {
+            map[target - array[i]] = i;  
+        }
+    }
+    return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
@@ -30,5 +41,5 @@ if (require.main === module) {
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
-
+hasTargetSum([1,3,7],10)
 module.exports = hasTargetSum;
